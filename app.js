8a00b70305532b35a9c4de1bbc5e490981966f48
Matt Haines <mhpahuta@gmail.com>
2022-03-19 16:30:45 -0700

    Rework getColors functionality, update styles, simplify markup

--- app.js
+++ app.js
@@ -1,74 +1,68 @@
-
 /*
-Basic fetch request format:
-fetch('http://example.com/movies.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
-
   Example scheme
   https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=html&mode=analogic&count=6
 
   Get a single hex value
   'https://www.thecolorapi.com/scheme?hex=0047AB&format=json&mode=analogic&count=5'
    
-  data => console.log(data.colors[0].hex.value
-
-  color modes:
-  monochrome 
-  monochrome-dark 
-  monochrome-light 
-  analogic 
-  complement
-  analogic-complement 
-  triad 
-
+  variables for: hex=${hexVal}, mode=${colorMode}
 */
-const baseURL = 'https://www.thecolorapi.com/';
-// variables for: hex=${hexVal}, mode=${colorMode}
+
 const colorInput = document.querySelector('#color-input');
 const schemeSelect = document.querySelector('#scheme-select');
+const countSelect = document.querySelector('#count-select');
 const getColorsBtn = document.querySelector('#get-scheme-btn');
 
+const colorResultsGrid = document.querySelector('#color-results-grid');
 
-// let hexVal = 'F55A5A';
-// let mode = 'monochrome';
+let seedColor = colorInput.value.substring(1);
+let colorMode = schemeSelect.value;
+// let colorCount = countSelect.value; // future feature
+// let colorsMarkup = ''; // change to colorsHTML - generate starter colors on page load?
 
-let colorsArray = [];
-
-// wrap this in a function - called when 'Get color scheme' button is clicked
-// 2 params - hexVal, mode
-function getColors(hexVal, mode) {
-  fetch(`https://www.thecolorapi.com/scheme?hex=${hexVal}&format=json&mode=${mode}`)
-    .then(res => res.json())
-    // .then(data => console.log(data.colors))
-    .then(data => data.colors.forEach(function (color) {
-      colorsArray.push(color.hex.value);
-    }))
+// get new seedColor value
+function getSeedColor(event) {
+  seedColor = event.target.value.substring(1); // use just schemSelect.value?
+  return seedColor;
 }
+colorInput.addEventListener('change', getSeedColor); // update val on change
 
-colorInput.addEventListener('change', () => {
-  colorsArray = [];
-})
+// get new colorMode value
+function getColorMode(event) {
+  colorMode = event.target.value;
+  return colorMode;
+}
+schemeSelect.addEventListener('change', getColorMode); // update val on change
 
-schemeSelect.addEventListener('change', () => {
-  colorsArray = [];
-})
+// function getColorCount(event) {
+//   colorCount = event.targe.value;
+//   return colorCount;
+// }
 
-
+// make API call on button click, generate colors grid html
+// seperate color generation into seperate function - renderColors (if possible)
 getColorsBtn.addEventListener('click', () => {
-  console.log(`Input val: ${colorInput.value}, Mode: ${schemeSelect.value}`)
-  getColors(colorInput.value.substring(1), schemeSelect.value)
+  fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&format=json&mode=${colorMode}&count=5`)
+    .then((res) => res.json())
+    .then((data) => {
+      let colorsMarkup = '' // check if need this
+      // use map method on data.colors array to generate html
+      data.colors.map((color, index) => {
+        colorsMarkup += `
+          <div id="bar-${index + 1}" class="color-bar" style="background-color: ${color.hex.value};">
+            <p style="display:none;">${color.hex.value}</p>
+          </div>
+          <div class="hex-code-div flex">
+            <p id="hex-${index + 1}">${color.hex.value}</p>
+          </div>
+          `
+        // return colorsMarkup // not needed?
+      })
+      colorResultsGrid.innerHTML = colorsMarkup;
+
+  })
 })
 
-  // .then(data => data.colors.forEach(function (color) {
-  //   console.log(color.hex.value)
-  // }));
 
-// colors[0].hex.value
 
-/*
-// event listener - color picked <input type="color" - "seed color"
-// event listener - color scheme <select> value
-// event listener - button - getColorScheme, make call to the API
-// function renderColors - render colors and hex values to the page
-*/
+
