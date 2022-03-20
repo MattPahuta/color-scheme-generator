b2d2017fe7f868e7172f9986ed34778b2da25b03
Matt Haines <mhpahuta@gmail.com>
2022-03-20 15:06:27 -0700

    Tidy up the code

--- app.js
+++ app.js
@@ -3,36 +3,34 @@ const schemeSelect = document.querySelector('#scheme-select');
 const countSelect = document.querySelector('#count-select')
 const getColorsBtn = document.querySelector('#get-scheme-btn');
 const colorResultsGrid = document.querySelector('#color-results-grid');
-
 let seedColor = colorInput.value.substring(1);
 let colorMode = schemeSelect.value;
 let colorCount = countSelect.value
-// let colorsMarkup = ''; // change to colorsHtml - generate starter colors on page load?
 
 // get new seedColor value
-function getSeedColor(event) {
-  seedColor = event.target.value.substring(1); // use just schemSelect.value?
+function getSeedColor(e) {
+  seedColor = e.target.value.substring(1);
   return seedColor;
 }
 colorInput.addEventListener('change', getSeedColor); // update val on change
 
 // get new colorMode value
-function getColorMode(event) {
-  colorMode = event.target.value;
+function getColorMode(e) {
+  colorMode = e.target.value;
   return colorMode;
 }
 schemeSelect.addEventListener('change', getColorMode); // update val on change
 
 // get number of colors
-function getColorCount(event) {
-  colorCount = event.target.value;
+function getColorCount(e) {
+  colorCount = e.target.value;
   return colorCount;
 }
 countSelect.addEventListener('change', getColorCount); // update val on change
 
 function addClickToCopy(hexCodes) {
   hexCodes.forEach((hexCode) => { // loop through and add listener
-    console.log(hexCode)
+    // console.log(hexCode) // debugging
     hexCode.addEventListener('click', (e) => {
       let copyText = e.target.textContent;
 
@@ -47,58 +45,26 @@ function generateColors() {
   fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&format=json&mode=${colorMode}&count=${colorCount}`)
     .then((res) => res.json())
     .then((data) => {
-      let colorsMarkup = '' // check if need this
-      console.log(colorCount)
+      let colorsHtml = ''
+      // console.log(colorCount) // debugging
       // use map method on data.colors array to generate html
       data.colors.map((color, index) => {
-        colorsMarkup += `
+        colorsHtml += `
           <div id="bar-${index + 1}" class="color-bar" style="background-color: ${color.hex.value};">
-            <p style="display:none;">${color.hex.value}</p>
+            <span style="display:none;">${color.hex.value}</span>
           </div>
           <div class="hex-code-div flex">
             <p id="hex-${index + 1}" class="hex-code">${color.hex.value}</p>
           </div>
           `
-        // return colorsMarkup // not needed?
       })
-      colorResultsGrid.innerHTML = colorsMarkup;
-
-    // add click/copy to color bar footer
-    const hexCodes = [...document.querySelectorAll('.hex-code')]; // spread to array
-    addClickToCopy(hexCodes);
-    
+      colorResultsGrid.innerHTML = colorsHtml;
+      // get generated hexCodes and pass to addClickToCopy function
+      const hexCodes = [...document.querySelectorAll('.hex-code')]; // spread to array
+      addClickToCopy(hexCodes);
   })
 }
 
-// make API call on button click, generate colors grid html
-// seperate color generation into seperate function - renderColors (if possible)
-// getColorsBtn.addEventListener('click', () => {
-//   fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&format=json&mode=${colorMode}&count=${colorCount}`)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       let colorsMarkup = '' // check if need this
-//       console.log(colorCount)
-//       // use map method on data.colors array to generate html
-//       data.colors.map((color, index) => {
-//         colorsMarkup += `
-//           <div id="bar-${index + 1}" class="color-bar" style="background-color: ${color.hex.value};">
-//             <p style="display:none;">${color.hex.value}</p>
-//           </div>
-//           <div class="hex-code-div flex">
-//             <p id="hex-${index + 1}" class="hex-code">${color.hex.value}</p>
-//           </div>
-//           `
-//         // return colorsMarkup // not needed?
-//       })
-//       colorResultsGrid.innerHTML = colorsMarkup;
-
-//     // add click/copy to color bar footer
-//     const hexCodes = [...document.querySelectorAll('.hex-code')]; // spread to array
-//     addClickToCopy(hexCodes);
-    
-//   })
-// })
-
+// add event listener to getColorsBtn
 getColorsBtn.addEventListener('click', generateColors);
-
 generateColors(); // call generateColors to set initial placeholder values
